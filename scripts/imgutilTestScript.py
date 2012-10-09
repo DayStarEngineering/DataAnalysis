f1290abc90080a2069aa2f5e77e677677326aae4
kevin <dinkelk@gmail.commm>
2012-10-09 09:59:55 -0600

    made sure Jeds script runs

--- scripts/imgutilTestScript.py
+++ scripts/imgutilTestScript.py
@@ -2,11 +2,13 @@
 # Script to load and save a tiff and dat file
 # 10/8/12
 
-from utilities import loadimg, saveimg
+import script_setup
+from util import imgutil as imgutil
+# from util import *
 
 tifpic = 'img_1348355543_717188_00015_00000_0_gray.tif'
 datpic = 'img_1348355543_717188_00015_00000_0.dat'
 
-saveimg(loadimg(tifpic),'tifpicsaved.tif')
-saveimg(loadimg(datpic),'datpicsaved.tif')
+imgutil.saveimg(imgutil.loadimg(tifpic),'tifpicsaved.tif')
+imgutil.saveimg(imgutil.loadimg(datpic),'datpicsaved.tif')
 
