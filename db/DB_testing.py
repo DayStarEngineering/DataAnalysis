12a2ad5dcdaa40816e4372d856bc710c5fff4721
ZachDischner <zach.dischner@gmail.com>
2012-10-13 12:33:18 -0600

    Now have return structure defined for a SELECT method. About to start trying PANDAS

--- db/DB_testing.py
+++ db/DB_testing.py
@@ -2,28 +2,48 @@ __author__ = 'zachdischner'
 
 #import mysql.connector
 import MySQLdb as MDB
+import numpy as NP
 import datetime
 import sys
 
 # Gotta import sys
-# sys.path.append('./')
+# sys.path.append('./')    - in top dir,    sys.path.append('./db/')
 # import DB_testing.py as db
 
-
 con = None
-
 print "Now trying to test out a script or something to connect to my database"
 
-def select(query,*args):
-#    cnx = mysql.connector.connect(user='root', database='test')
-    cnx =MDB.connect(user='root', database='test')
-    cursor = cnx.cursor()
 
-    cursor.execute(query, args)
-    for result in cursor:
-        yield result
-    cursor.close()
-    cnx.close()
+#from collections import namedtuple
+#>>> xx=namedtuple("xx",x[0].keys())
+
+
+
+def select(query,*args):
+    try:
+#    cnx = mysql.connector.connect(user='root', database='test')
+        cnx = MDB.connect(host='localhost', user='root', passwd='', db='test')    # host, user, password, database
+        cursor = cnx.cursor(MDB.cursors.DictCursor)
+
+        cursor.execute(query, args)
+    #    for result in cursor:
+    #        yield result
+        result = cursor.fetchallDict()
+
+        cursor.close()
+        cnx.close()
+        return result
+
+    except MDB.Error, e:
+        print "Error %d: %s" % (e.args[0], e.args[1])
+        sys.exit(1)
+
+    finally:
+        if cnx.open:
+            cnx.close()
+
+# result[0]['volume']   => 10000. Yay!!!
+
 
 #try:
 #
@@ -44,3 +64,9 @@ def select(query,*args):
 #
 #    if con:
 #        con.close()
+
+
+
+
+#select('SELECT * FROM STOCK_TEST LIMIT 1')
+
