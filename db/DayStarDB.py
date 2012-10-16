f88dde0ed8719987f0a096c5c3bde757cb07bf2b
ZachDischner <zach.dischner@gmail.com>
2012-10-16 00:40:52 -0600

    Now Table creation, checking, definitions added

--- db/DayStarDB.py
+++ db/DayStarDB.py
@@ -1,14 +1,14 @@
 __author__ = 'zachdischner'
 
+# Might want to get rid of EXECUTE_STATEMENT, SELECT with Pandas seems to work much better
 
-#import mysql.connector
 import MySQLdb as MDB
 import numpy as NP
 import datetime
 import sys
 import pandas.io.sql as psql
 
-class DatabaseConn:
+class DatabaseConnect:
     def __init__(self, environment='stock_test',default_table='stock_test',debug=1,):
         self.env = environment
         self.debug=debug
@@ -18,7 +18,7 @@ class DatabaseConn:
     # Gotta import sys
     # sys.path.append('./db/')
     # import DB_testing as db
-    # testdb=db.DatabaseConn)()
+    # testdb=db.DatabaseConnect)()
 
     #con = None
     #print "Now trying to test out a script or something to connect to my database"
@@ -30,6 +30,19 @@ class DatabaseConn:
     #from collections import namedtuple
     #>>> xx=namedtuple("xx",x[0].keys())
 
+    #*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^* class_info *^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*
+    #
+    #   Purpose: Print out all class attributes. Gives insight into current class defaults
+    #
+    #   Inputs:  {} None
+    #
+    #   Outputs: {} No variable return. Just print statements in the python window
+    #
+    #   Usage  : >>> import DayStarDB as DayStar
+    #            >>> db = Daystar.DatabaseConnect({optional specs})
+    #            >>> db.class_info()
+    #
+    #<*>---<*>---<*>---<*>---<*>---<*>---<*>---<*>---<*>---<*>---<*>---<*>---<*>---<*>---<*>---<*>
 
 
     def class_info(self):
@@ -48,17 +61,20 @@ class DatabaseConn:
     #   Outputs: {con} - A database connection object(?). With it, connect to, update, and retrive
     #                    data from the database, as specified by the 'env' variable.
     #
-    #   Usage  : >>> import DB_testing as db
-    #            >>> my_connection=db.connect()
+    #   Usage  : >>> import DayStarDB as DayStar
+    #            >>> db = Daystar.DatabaseConnect({optional specs})
+    #            >>> my_connection=db.connect('my_environment')
     #
     #<*>---<*>---<*>---<*>---<*>---<*>---<*>---<*>---<*>---<*>---<*>---<*>---<*>---<*>---<*>---<*>
-    def MySQLconnect(self,env):
-    # Homemade 'switch' statement. Connect automagically to the database environment of choice
+    def MySQLconnect(self,env=None):
+        if env is None:
+            env = self.env
+            # Homemade 'switch' statement. Connect automagically to the database environment of choice
         con = {
-                  'stock_test': MDB.connect(host='localhost', user='root', passwd='', db='test'),
-                  'development': 2,
-                  'production': 3
-                  }
+            'stock_test': MDB.connect(host='localhost', user='root', passwd='', db='test'),
+            'development': 2,
+            'production': 3
+        }
         if env in con:
             connection= con[env]
         else:
@@ -82,6 +98,9 @@ class DatabaseConn:
         return value
 
 
+
+
+
     def find(self,what,where,table=None):
         if table is None:
             table = self.default_table
@@ -98,13 +117,38 @@ class DatabaseConn:
         return self.select(select_sql)
 
 
-    def create_database(self,DBname):
+    def show_databases(self,to_list=0):
+        show_sql="SHOW DATABASES"
+        databases = self.select(show_sql)
+        if to_list:
+            return databases.Database.tolist()
+        else:
+            return databases
+
+    def show_tables(self,to_list=0):
+        show_sql="SHOW TABLES"
+        tables = self.select(show_sql)
+        if to_list:
+            return tables.Tables_in_test.tolist()
+        else:
+            return tables
+
+    def execute_statement(self,query):
+        if self.debug > 0:
+            print query
         con = self.MySQLconnect(self.env)
         cur = con.cursor()
-        create_sql = 'CREATE DATABASE IF NOT EXISTS %s' % DBname
-        cur.execute(create_sql)
+        cur.execute(query)
         cur.close()
         con.close()
+
+
+
+    def create_database(self,DBname):
+
+        create_sql = 'CREATE DATABASE IF NOT EXISTS %s' % DBname
+        self.execute_statement(create_sql)
+
         # Verify that the database was created
         convert_to_list = 1
         if DBname in self.show_databases(convert_to_list):
@@ -123,11 +167,7 @@ class DatabaseConn:
         prompt = "You are about to DestroyExplode the database   [:%s]  Are you sure? [Yes/No]" % DBname
         if self.verify(prompt):
             drop_sql = "DROP DATABASE IF EXISTS %s" % DBname
-            con = self.MySQLconnect(self.env)
-            cur = con.cursor()
-            cur.execute(drop_sql)
-            cur.close()
-            con.close()
+            self.execute_statement(drop_sql)
 
             # Verify
             convert_to_list = 1
@@ -147,13 +187,7 @@ class DatabaseConn:
             print "Good plan. Just do it later bro"
 
 
-    def show_databases(self,to_list=0):
-        show_sql="SHOW DATABASES"
-        databases = self.select(show_sql)
-        if to_list:
-            return databases.Database.tolist()
-        else:
-            return databases
+
 
 
 
@@ -175,34 +209,50 @@ class DatabaseConn:
 
 
 
-    def create_table(key):
+    def create_table(self,TableName):
+        if TableName.__class__ is not str:
+            print "Single string input to DayStarDB.create_table. Reconsider your arguments"
+            return -1
+        print "You are about to (delete and re)create Table   [%s]   " % TableName
 
-        def switcher(key):
-            return{
-                ''
+        TableName=TableName.lower()
 
-            }
+        create_keys ={
+            'rawdata':'CREATE TABLE rawdata ('+
+                      'id BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,'    # Identifier
+                      'raw_fn VARCHAR(100) NOT NULL,'                      # Raw, unprocessed filename
+                      'seconds INT(10),'                                   # Seconds since 1970. 6 digit
+                      'usec INT(6),'                                       # usec since last second
+                      'burst_num INT(4),'                                  # Burst number in sequence
+                      'image_num INT(5),'                                  # Image number in burst
+                      'gain boolean,'                                      # Gain. 1=high, low=0
+                      'exposure INT(4),'
+                      'time TIME,'                                          #(hours, minutes, seconds)-insert into foo (time) values("4:54:32");, must round to nearest second
+                      'PRIMARY KEY(id))'
+        }
 
+        if TableName not in create_keys.keys():
+            print 'Table definition for   [%s]   has not been defined. ' % TableName
+            print 'Update DayStarDB.create_table, or create it yourself'
+            return -1
 
-#    def select(query, *args):
-#
-#        try:
-#            cnx = self.MySQLconnect()
-#            cursor = cnx.cursor(MDB.cursors.DictCursor)
-#            cursor.execute(query, args)
-#            result = cursor.fetchallDict()
-#            cursor.close()
-#            cnx.close()
-#            return result
-#
-#        except MDB.Error, e:
-#            print "Error %d: %s" % (e.args[0], e.args[1])
-#            sys.exit(1)
-#
-#        finally:
-#            if cnx.open:
-#                cnx.close()
+        drop_table_sql = 'DROP TABLE IF EXISTS %s ;' % TableName
+        self.execute_statement(drop_table_sql)
+        # Verify The Drop
+        convert_to_list = 1
+        if TableName in self.show_tables(convert_to_list):
+            print "Uh oh, table   [%s]   was not dropped correctly. It still exists" % TableName
+        else:
+            print "Table   [%s]   was dropped successfully " % TableName
 
+        create_table_sql = create_keys[TableName]
+        self.execute_statement(create_table_sql)
 
-                # result[0]['volume']   => 10000. Yay!!!
+        # Verify Table Existence
+        convert_to_list = 1
+        if TableName in self.show_tables(convert_to_list):
+            print "Table  [%s]  Was created successfully!" % TableName
+        else:
+            print "Table   [%s]   was dropped successfully " % TableName
+
 
