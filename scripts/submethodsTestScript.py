40f36bca14dbebf4087373e19383b472fc605007
Jed Diller <jedediah.diller@colorado.edu>
2012-10-25 15:38:14 -0600

    changed diplaying scale to be in 16 bit range, added new centroid matching util

--- scripts/submethodsTestScript.py
+++ scripts/submethodsTestScript.py
@@ -8,23 +8,29 @@ from util import submethods as submethods
 
 import numpy as np
 
+# image path setup
 datpic = '../testimgdir/img_1348355543_717188_00015_00000_0.dat'
-DRCMoutfile = '../testimgdir/DRCMsubdimg.tif'   # dark row column average subtraction
-CMoutfile = '../testimgdir/CMsubdimg.tif'       # column average subtraction
+#DRCMoutfile = '../testimgdir/DRCMsubdimg.tif'   # dark row column average subtraction
+#CMoutfile = '../testimgdir/CM10xsubdimg.tif'       # column average subtraction
+#CSMoutfile = '../testimgdir/CM10xsubdimg.tif'       # column sigma range average subtraction
 
 # load
+print 'loading image...'
 img = imgutil.loadimg(datpic)
-fullimg = imgutil.loadimg(datpic,'full')
+#fullimg = imgutil.loadimg(datpic,'full')
 
 # do subtractions
-DRCMimg = submethods.darkcolsub(fullimg)
+print 'doing sigma subtraction...'
+#DRCMimg = submethods.darkcolsub(fullimg)
 CMimg = submethods.colmeansub(img)
+#CSMimg = submethods.colsigsub(img)
 
 # display images
-imgutil.dispimg(DRCMimg)
-imgutil.dispimg(CMimg,10) # displayed with view factor of 10
+#imgutil.dispimg(DRCMimg)
+print 'displaying image...'
+imgutil.dispimg(CMimg,10) # displayed with an optional view factor of 10
 
 # save images
 #imgutil.saveimg(DRCMimg,DRCMoutfile)
-#imgutil.saveimg(CMimg,CMoutfile)
+#imgutil.saveimg(CMimg,CMoutfile,10) # saved with an optional view factor of 10
 
